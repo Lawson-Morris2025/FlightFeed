c2623c18d679035d9f0f2d790c83639d2c18b9fa
Lawson-Morris2025 <morrislawson11@outlook.com>
2025-11-02 22:02:18 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'flightfeed-cache-v3';
+const CACHE_NAME = 'flightfeed-cache-v4';
 
 // On install, pre-cache the main app shell files.
 self.addEventListener('install', event => {
